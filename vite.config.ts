0cc7121d45cbe7142ba613a7de14072b8bb57b62
Cuong <cuong@foxit.co.jp>
2024-12-20 13:38:56 +0700

    add sitemap and google site verify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://daydamdui.com",
+    }),
+  ],
   build: {
     outDir: "dist",
   },
