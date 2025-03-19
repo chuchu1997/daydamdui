a076917260ee8532a3f3c2c2ef3f0c9f27860951
cuong <cuong@foxit.co.jp>
2025-03-19 14:28:55 +0700

    try again

--- src/views/about.tsx
+++ src/views/about.tsx
@@ -1,5 +1,5 @@
 
 // eslint-disable-next-line no-empty-pattern
 export const AboutView = () => {
-  return <div>This is About view 111 111 </div>;
+  return <div>This is About view 111 111 123 </div>;
 };
